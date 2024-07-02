mvp79-dev/submitty

commit 3dbc37c95f2b37db46c8f4900afbd36f6a5a8280

[DevDependency] Update ESLint style rules (#10551)
This PR replaces the [deprecated ESLint style
rules](https://eslint.org/blog/2023/10/deprecating-formatting-rules/) in
our ESLint config to the newer, maintained ones by [ESLint
Stylistic](eslint.style). Since the previous rules were broken and not
working properly, this PR also updates any non-conformant files to be
conformant with our style rules.

This PR also changes our
[arrow-parens](https://eslint.style/rules/js/arrow-parens) style rule to
"always".

---------

Co-authored-by: William Powe <116031952+powe97@users.noreply.github.com>

site/public/js/rainbow-customization.js
@@ -13,7 +13,7 @@ function ExtractBuckets() {
     $('#custom_form').submit();
 }
 
-//Forces element's value to be non-negative
+// Forces element's value to be non-negative
 // eslint-disable-next-line no-unused-vars
 function ClampPoints(el) {
     if (el.value === '') {
@@ -37,8 +37,8 @@ function ExtractBucketName(s, offset) {
     const tmp = s.split('-');
     let bucket = '';
     let i;
-    for (i=offset;i<tmp.length; i++) {
-        if (i>offset) {
+    for (i = offset; i < tmp.length; i++) {
+        if (i > offset) {
             bucket += '-';
         }
         bucket += tmp[i];
@@ -46,7 +46,7 @@ function ExtractBucketName(s, offset) {
     return bucket;
 }
 
-//Forces element's value to be in range [0.0,100.0]
+// Forces element's value to be in range [0.0,100.0]
 // eslint-disable-next-line no-unused-vars
 function ClampPercent(el) {
     el.value = Math.min(Math.max(el.value, 0.0), 100.0);
@@ -54,35 +54,35 @@ function ClampPercent(el) {
     $(`#config-percent-${ExtractBucketName(el.id, 1)}`).text(`${el.value}%`);
 }
 
-//Updates the sum of percentage points accounted for by the buckets being used
+// Updates the sum of percentage points accounted for by the buckets being used
 function UpdateUsedPercentage() {
     let val = 0.0;
-    $("input[id^='percent']").filter(function() {
+    $("input[id^='percent']").filter(function () {
         return $(this).parent().css('display') !== 'none';
-    }).each(function() {
+    }).each(function () {
         val += parseFloat($(this).val());
     });
     const percentage_span = $('#used_percentage');
     percentage_span.text(`${val.toString()}%`);
-    if (val>100.0) {
-        percentage_span.css({'color':'red', 'font-weight':'bold'});
+    if (val > 100.0) {
+        percentage_span.css({ 'color': 'red', 'font-weight': 'bold' });
     }
     else {
-        percentage_span.css({'color':'var(--text-black)', 'font-weight':''});
+        percentage_span.css({ 'color': 'var(--text-black)', 'font-weight': '' });
     }
 }
 
-//Updates which buckets have full configuration shown (inc. each gradeable), and the ordering
+// Updates which buckets have full configuration shown (inc. each gradeable), and the ordering
 // eslint-disable-next-line no-unused-vars
 function UpdateVisibilityBuckets() {
-    //For each bucket that isn't being used, hide it
-    $('#buckets_available_list').find('input').each(function() {
-        //Extract the bucket name
+    // For each bucket that isn't being used, hide it
+    $('#buckets_available_list').find('input').each(function () {
+        // Extract the bucket name
         const bucket = ExtractBucketName($(this).attr('id'), 1);
         $(`#config-${bucket}`).css('display', 'none');
     });
 
-    //For each bucket that IS being used, show it
+    // For each bucket that IS being used, show it
     const used_buckets = $('#buckets_used_list').find('input');
     if (used_buckets.length === 0) {
         return;
@@ -90,8 +90,8 @@ function UpdateVisibilityBuckets() {
     let prev_bucket = ExtractBucketName(used_buckets.first().attr('id'), 1);
     $(`#config-${prev_bucket}`).prependTo('#config-wrapper').css('display', 'block');
 
-    used_buckets.each(function() {
-        //Extract the bucket name
+    used_buckets.each(function () {
+        // Extract the bucket name
         const bucket = ExtractBucketName($(this).attr('id'), 1);
         console.log(`prev_bucket: ${prev_bucket} bucket: ${bucket}`);
         if (bucket !== prev_bucket) {
@@ -106,20 +106,18 @@ function getDisplay() {
     // Collect display
     const display = [];
 
-    $.each($("input[name='display']:checked"), function() {
+    $.each($("input[name='display']:checked"), function () {
         display.push($(this).val());
     });
 
     return display;
 }
 
-
 function getSection() {
     // Collect sections and labels
     const sections = {};
 
-    $.each($("input[class='sections_and_labels']"), function() {
-
+    $.each($("input[class='sections_and_labels']"), function () {
         // Get data
         const section = this.getAttribute('data-section').toString();
         const label = this.value;
@@ -139,7 +137,7 @@ function getDisplayBenchmark() {
     // Collect display benchmarks
     const display_benchmarks = [];
 
-    $.each($("input[name='display_benchmarks']:checked"), function() {
+    $.each($("input[name='display_benchmarks']:checked"), function () {
         display_benchmarks.push($(this).val());
     });
 
@@ -169,8 +167,7 @@ function getSelectedCurveBenchmarks() {
 function getGradeableBuckets() {
     // Collect gradeable buckets
     const gradeables = [];
-    $('.bucket_detail_div').each(function() {
-
+    $('.bucket_detail_div').each(function () {
         // Only use buckets which have display block
         // This works even if outer container is collapsed
         if ($(this).css('display') === 'block') {
@@ -197,8 +194,7 @@ function getGradeableBuckets() {
             // Extract each independent gradeable in the bucket
             const ids = [];
             const selector = `#gradeables-list-${type}`;
-            $(selector).children('.gradeable-li').each(function() {
-
+            $(selector).children('.gradeable-li').each(function () {
                 const gradeable = {};
 
                 const children = $(this).children();
@@ -215,8 +211,7 @@ function getGradeableBuckets() {
                 // Get per-gradeable curve data
                 const curve_points_selected = getSelectedCurveBenchmarks();
 
-                $(children).find('.gradeable-li-curve input').each(function() {
-
+                $(children).find('.gradeable-li-curve input').each(function () {
                     const benchmark = this.getAttribute('data-benchmark').toString();
 
                     if (curve_points_selected.includes(benchmark) && this.value) {
@@ -230,7 +225,6 @@ function getGradeableBuckets() {
 
                 // Validate the set of per-gradeable curve values
                 if (Object.prototype.hasOwnProperty.call(gradeable, 'curve')) {
-
                     // Has correct number of values
                     if (gradeable.curve.length !== curve_points_selected.length) {
                         throw `To adjust the curve for gradeable ${gradeable.id} you must enter a value in each box`;
@@ -238,7 +232,6 @@ function getGradeableBuckets() {
 
                     let previous = gradeable.max;
                     gradeable.curve.forEach((elem) => {
-
                         elem = parseFloat(elem);
 
                         // All values are floats
@@ -308,7 +301,7 @@ function addToTable() {
     }
 
     // eslint-disable-next-line no-undef
-    const studentFullDataValues = studentFullData.map(item => item.value);
+    const studentFullDataValues = studentFullData.map((item) => item.value);
     if (!studentFullDataValues.includes(USERID)) {
         alert('Invalid User ID. Please enter a valid one.');
         return;
@@ -319,7 +312,6 @@ function addToTable() {
         alert('Penalty must be between 0 - 1');
         return;
     }
-
 
     const tableBody = document.getElementById('table-body');
 
@@ -386,14 +378,12 @@ function getBenchmarkPercent() {
     const benchmark_percent = {};
     const selected_benchmarks = getSelectedCurveBenchmarks();
 
-    $('.benchmark_percent_input').each(function() {
-
+    $('.benchmark_percent_input').each(function () {
         // Get data
         const benchmark = this.getAttribute('data-benchmark').toString();
         const percent = this.value;
 
         if (selected_benchmarks.includes(benchmark)) {
-
             // Verify percent is not empty
             if (percent === '') {
                 throw 'All benchmark percents must have a value before saving.';
@@ -406,7 +396,6 @@ function getBenchmarkPercent() {
 
             // Add to sections
             benchmark_percent[benchmark] = percent;
-
         }
     });
 
@@ -434,14 +423,12 @@ function getFinalCutoffPercent() {
     const final_cutoff = {};
     const letter_grades = ['A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'D+', 'D'];
 
-    $('.final_cutoff_input').each(function() {
-
+    $('.final_cutoff_input').each(() => {
         // Get data
         const letter_grade = this.getAttribute('data-benchmark').toString();
         const percent = this.value;
 
         if (letter_grades.includes(letter_grade)) {
-
             // Verify percent is not empty
             if (percent === '') {
                 throw 'All final cutoffs must have a value before saving.';
@@ -454,7 +441,6 @@ function getFinalCutoffPercent() {
 
             // Add to sections
             final_cutoff[letter_grade] = percent;
-
         }
     });
 
@@ -463,17 +449,15 @@ function getFinalCutoffPercent() {
 
 // This function constructs a JSON representation of all the form input
 function buildJSON() {
-
     // Build the overall json
     let ret = {
-        'display': getDisplay(),
-        'display_benchmark': getDisplayBenchmark(),
-        'benchmark_percent': getBenchmarkPercent(),
-        'final_cutoff': getFinalCutoffPercent(),
-        'section' : getSection(),
-        'gradeables' : getGradeableBuckets(),
-        'messages' : getMessages(),
-        'plagiarism' : getPlagiarism(),
+        display: getDisplay(),
+        display_benchmark: getDisplayBenchmark(),
+        benchmark_percent: getBenchmarkPercent(),
+        section: getSection(),
+        gradeables: getGradeableBuckets(),
+        messages: getMessages(),
+        plagiarism: getPlagiarism(),
     };
 
     ret = JSON.stringify(ret);
@@ -493,25 +477,19 @@ function checkAutoRGStatus() {
         // eslint-disable-next-line no-undef
         url: buildCourseUrl(['reports', 'rainbow_grades_status']),
         // eslint-disable-next-line no-undef
-        data: {csrf_token: csrfToken},
+        data: { csrf_token: csrfToken },
         success: function (response) {
             if (response.status === 'success') {
-
                 $('#save_status').html('Rainbow grades successfully generated!');
                 showLogButton(response.data);
-
             }
             else if (response.status === 'fail') {
-
                 $('#save_status').html('A failure occurred generating rainbow grades');
                 showLogButton(response.message);
-
             }
             else {
-
                 $('#save_status').html('Internal Server Error');
                 console.log(response);
-
             }
         },
         error: function (response) {
@@ -520,11 +498,9 @@ function checkAutoRGStatus() {
     });
 }
 
-
-//This function attempts to create a new customization.json server-side based on form input
+// This function attempts to create a new customization.json server-side based on form input
 // eslint-disable-next-line no-unused-vars
 function ajaxUpdateJSON(successCallback, errorCallback) {
-
     try {
         $('#save_status').html('Saving...');
 
@@ -534,18 +510,18 @@ function ajaxUpdateJSON(successCallback, errorCallback) {
             type: 'POST',
             url: url,
             // eslint-disable-next-line no-undef
-            data: {json_string: buildJSON(), csrf_token: csrfToken},
+            data: { json_string: buildJSON(), csrf_token: csrfToken },
             success: function (response) {
                 if (response.status === 'success') {
                     $('#save_status').html('Generating rainbow grades, please wait...');
 
                     // Call the server to see if auto_rainbow_grades has completed
                     checkAutoRGStatus();
-                    //successCallback(response.data);
+                    // successCallback(response.data);
                 }
                 else if (response.status === 'fail') {
                     $('#save_status').html('A failure occurred saving customization data');
-                    //errorCallback(response.message, response.data);
+                    // errorCallback(response.message, response.data);
                 }
                 else {
                     $('#save_status').html('Internal Server Error');
@@ -561,7 +537,6 @@ function ajaxUpdateJSON(successCallback, errorCallback) {
         $('#save_status').html(err);
     }
 }
-
 
 function displayChangeDetectedMessage() {
     $('#save_status').text('Changes detected, press "Save Changes" to save them.');
@@ -604,11 +579,11 @@ function setInputsVisibility(elem) {
  * based on the corresponding boxes in 'display' being selected / un-selected
  * */
 function setCustomizationItemVisibility(elem) {
-    //maps a checkbox name to the corresponding customization item id
+    // maps a checkbox name to the corresponding customization item id
     const checkbox_to_cust_item = {
-        'final_grade': '#final_grade_cutoffs',
-        'messages': '#cust_messages',
-        'section': '#section_labels',
+        final_grade: '#final_grade_cutoffs',
+        messages: '#cust_messages',
+        section: '#section_labels',
     };
     const checkbox_name = elem.value;
     const cust_item_id = checkbox_to_cust_item[checkbox_name];
@@ -623,21 +598,19 @@ function setCustomizationItemVisibility(elem) {
 }
 
 $(document).ready(() => {
-
     // Make the per-gradeable curve inputs toggle when the icon is clicked
     // eslint-disable-next-line no-unused-vars
-    $('.fa-gradeable-curve').click(function(event) {
+    $('.fa-gradeable-curve').click(function (event) {
         const id = jQuery(this).attr('id').split('-')[3];
         $(`#gradeable-curve-div-${id}`).toggle();
     });
 
     // By default, open the input fields for per-gradable curves which have been previously set
-    $('.gradeable-li-curve').each(function() {
-
+    $('.gradeable-li-curve').each(function () {
         let has_at_least_one_value = false;
 
         // Determine if any of the input boxes had a value pre-loaded into them
-        $(this).children('input').each(function() {
+        $(this).children('input').each(function () {
             if (this.value) {
                 has_at_least_one_value = true;
             }
@@ -655,13 +628,12 @@ $(document).ready(() => {
      * Curve input boxes include the benchmark percent input boxes and also the per-gradeable curve input boxes
      * Visibility is controlled by which boxes are selected in the display benchmarks area
      */
-    $('#display_benchmarks input').each(function() {
-
+    $('#display_benchmarks input').each(function () {
         // Set the initial visibility on load
         setInputsVisibility(this);
 
         // Register a click handler to adjust visibility when boxes are selected / un-selected
-        $(this).change(function() {
+        $(this).change(function () {
             setInputsVisibility(this);
         });
     });
@@ -671,18 +643,16 @@ $(document).ready(() => {
      * Visibility is controlled by whether the corresponding boxes are selected in the display area
      */
     const dropdown_checkboxes = ['final_grade', 'messages', 'section'];
-    $('#display input').each(function() {
-
+    $('#display input').each(function () {
         if (dropdown_checkboxes.includes(this.value)) {
             // Set the initial visibility on load
             setCustomizationItemVisibility(this);
 
             // Register a click handler to adjust visibility when boxes are selected / un-selected
-            $(this).change(function() {
+            $(this).change(function () {
                 setCustomizationItemVisibility(this);
             });
         }
-
     });
 
     $("input[name*='display']").change(() => {
@@ -710,7 +680,6 @@ $(document).ready(() => {
         displayChangeDetectedMessage();
     });
 
-
     // https://stackoverflow.com/questions/15657686/jquery-event-detect-changes-to-the-html-text-of-a-div
     // More Details https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
     // select the target node
@@ -737,7 +706,6 @@ $(document).ready(() => {
     });
 });
 
-
 $(document).ready(() => {
     $('#pencilIcon').click((event) => {
         event.stopPropagation();
@@ -748,7 +716,7 @@ $(document).ready(() => {
             ? checkboxControls.show()
             : checkboxControls.hide() && dropLowestDiv.hide();
     });
-    $('#drop_lowest_checkbox').change(function(event) {
+    $('#drop_lowest_checkbox').change(function (event) {
         event.stopPropagation();
         const dropLowestDivs = $('div[id^="dropLowestDiv-"]');
         const isChecked = $(this).is(':checked');

commit 8779ce36b5b7eac74d5f8a71244a927cfc1088ca

[Bugfix:RainbowGrades] Fix final cutoff customization (#10652)
### Please check if the PR fulfills these requirements:

* [ ] Tests for the changes have been added/updated (if possible)
* [ ] Documentation has been updated/added if relevant
* [ ] Screenshots are attached to Github PR if visual/UI changes were
made

Login to Instructor, then navigate to Grade Reports > Web-Based Rainbow
Grades Customization.

### What is the current behavior?
<!-- List issue if it fixes/closes/implements one using the "Fixes
#<number>" or "Closes #<number>" syntax -->
The following error on the Rainbow Grades customization page was caused
by a refactor in PR #10551 (Update ESLint style rules).

![error](https://github.com/Submitty/Submitty/assets/56311867/c3b80163-5075-4f3f-9c7b-a358ca477e75)

### What is the new behavior?
Final Cutoffs can once again be set using the Rainbow Grades
customization page.

Co-authored-by: Williams Chen <82904790+williamschen23@users.noreply.github.com>

site/public/js/rainbow-customization.js
@@ -423,7 +423,7 @@ function getFinalCutoffPercent() {
     const final_cutoff = {};
     const letter_grades = ['A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'D+', 'D'];
 
-    $('.final_cutoff_input').each(() => {
+    $('.final_cutoff_input').each(function () {
         // Get data
         const letter_grade = this.getAttribute('data-benchmark').toString();
         const percent = this.value;
@@ -454,6 +454,7 @@ function buildJSON() {
         display: getDisplay(),
         display_benchmark: getDisplayBenchmark(),
         benchmark_percent: getBenchmarkPercent(),
+        final_cutoff: getFinalCutoffPercent(),
         section: getSection(),
         gradeables: getGradeableBuckets(),
         messages: getMessages(),