mvp79-dev/submitty

commit 6c3a186dba6b1c30ef8abab94a7e7e61416069d5

[Feature:RainbowGrades] Add Gradeable Percents to GUI (#10763)
### Please check if the PR fulfills these requirements:

* [ ] Tests for the changes have been added/updated (if possible)
* [ ] Documentation has been updated/added if relevant
* [ ] Screenshots are attached to Github PR if visual/UI changes were
made

Login to Instructor, then navigate to Grade Reports > Web-Based Rainbow
Grades Customization.

### What is the new behavior?
Added a checkbox labeled _Per Gradeable Percents_ under the Gradeables
configuration, that adds textboxes for percents for each gradeable. This
allows final grade percent configuration per gradeable in the
RainbowGrades GUI.


![image](https://github.com/user-attachments/assets/a39c5cb0-a645-4eb4-808e-82d41babfb07)

### Other information?
<!-- Is this a breaking change? -->
<!-- How did you test -->

site/app/models/RainbowCustomization.php
@@ -103,7 +103,8 @@ class RainbowCustomization extends AbstractModel {
                 "max_score" => $max_score,
                 "grade_release_date" => $gradeable->hasReleaseDate() ? DateUtils::dateTimeToString($gradeable->getGradeReleasedDate()) : DateUtils::dateTimeToString($gradeable->getSubmissionOpenDate()),
                 "override" => false,
-                "override_max" => $max_score
+                "override_max" => $max_score,
+                "override_percent" => false
             ];
         }
 
@@ -154,6 +155,10 @@ class RainbowCustomization extends AbstractModel {
                     elseif ($c_gradeable['max_score'] !== (float) $json_bucket->ids[$j_index]->max) {
                         $c_gradeable['override'] = true;
                         $c_gradeable['override_max'] = $json_bucket->ids[$j_index]->max;
+                    }
+                    if (property_exists($json_bucket->ids[$j_index], 'percent')) {
+                        $c_gradeable['override_percent'] = true;
+                        $c_gradeable['percent'] = ($json_bucket->ids[$j_index]->percent) * 100;
                     }
                     $j_index++;
                 }

site/app/templates/admin/RainbowCustomization.twig
@@ -167,16 +167,16 @@
                         <i id="pencilIcon" class="fas fa-pencil-alt black-btn"></i>
                     </h2>
                     <div id="checkboxControls" style="display: none;">
-                        <label>Extra credit:
-                            <input id="extra_credit_checkbox" type="checkbox" name="extra_credit">
-                        </label><br>
+                        <input id="extra_credit_checkbox" type="checkbox" name="extra_credit">
+                        <label for="extra_credit_checkbox">Extra credit</label><br>
 
-                        <label>Remove lowest:
-                            <input id="drop_lowest_checkbox" type="checkbox" name="drop_lowest">
-                        </label>
+                        <input id="drop_lowest_checkbox" type="checkbox" name="drop_lowest">
+                        <label for="drop_lowest_checkbox">Remove lowest</label><br>
+
+                        <input id="enable-per-gradeable-percents" type="checkbox" name="enable-per-gradeable-percents">
+                        <label for="enable-per-gradeable-percents">Enable/Edit Per Gradeable Percents:</label>
+                        <i>For each bucket, you may grade by percent instead of by points.</i><br>
                     </div>
-
-
                     <div id="config-wrapper">
                         {% for bucket, gradeables in customization_data %}
                             <div id="config-{{ bucket }}" style="margin-top:25px;min-height:100px;display:none;" class="bucket_detail_div">
@@ -192,6 +192,22 @@
                                     >
                                     items
                                     <span style="margin-left: 100px;"></span>
+
+                                    {% set per_gradeable_percents_checked = false %}
+                                    {% for gradeable in gradeables %}
+                                        {% if gradeable["override_percent"] %}
+                                            {% set per_gradeable_percents_checked = true %}
+                                        {% endif %}
+                                    {% endfor %}
+                                    <i id="per-gradeable-percents-warning-{{ bucket }}" class="fas fa-exclamation-triangle fa-rb tooltip">
+                                        <span class="tooltiptext tooltipbottom">{# Message composed in JavaScript #}</span>
+                                    </i>
+                                    <input id="per-gradeable-percents-checkbox-{{ bucket }}" type="checkbox" name="per-gradeable-percents-{{ bucket }}" {{ per_gradeable_percents_checked ? 'checked' : '' }}>
+                                    <label id="per-gradeable-percents-label-{{ bucket }}" for="per-gradeable-percents-checkbox-{{ bucket }}">Per Gradeable Percents</label>
+                                    <span style="margin-left: 10px;"></span>
+                                    <button class="btn" id="per-gradeable-percents-reset-{{ bucket }}" type="button" onclick="ResetPerGradeablePercents('{{ bucket }}');">Even Split</button>
+                                    <span style="margin-left: 100px;"></span>
+
                                     <div id="dropLowestDiv-{{ bucket }}" style="display: none;">
                                         <label>
                                             Remove lowest:
@@ -210,17 +226,29 @@
                                     <ol style="list-style-type: none;" id="gradeables-list-{{ bucket }}">
                                         {% for gradeable in gradeables %}
                                             <li class="gradeable-li pt-1" style="border-top: 1px solid #FFFFFF;">
-                                                <input type="text"
-                                                       class="gradeable-custom-item-input"
-                                                       aria-label="{{gradeable["title"]}} max score"
-                                                       class="{{ gradeable["override"] ? "override" : "" }}"
-                                                       placeholder="{{ gradeable["max_score"] }}"
-                                                       value="{{ gradeable["override"] ? gradeable["override_max"] : gradeable["max_score"] }}"
-                                                       id="maxscore-{{ gradeable["id"] }}"
-                                                       data-grade-release-date="{{ gradeable["grade_release_date"] }}"
-                                                       style="margin-right: 10px;"
-                                                       onblur="ClampPoints(this);"
-                                                       onkeyup="DetectMaxOverride(this);">
+                                                <div id="gradeable-pts-div-{{ gradeable["id"] }}">
+                                                    <input type="text"
+                                                           class="gradeable-custom-item-input {{ gradeable["override"] ? "override" : "" }}"
+                                                           aria-label="{{gradeable["title"]}} max score"
+                                                           placeholder="{{ gradeable["max_score"] }}"
+                                                           value="{{ gradeable["override"] ? gradeable["override_max"] : gradeable["max_score"] }}"
+                                                           id="maxscore-{{ gradeable["id"] }}"
+                                                           data-grade-release-date="{{ gradeable["grade_release_date"] }}"
+                                                           onblur="ClampPoints(this);"
+                                                           onkeyup="DetectMaxOverride(this);">
+                                                    <span>pts</span>
+                                                </div>
+                                                <div id="gradeable-percents-div-{{ bucket }}-{{ gradeable["id"] }}">
+                                                    <input type="text"
+                                                           {% set default_percent = ((1 / gradeables|length) * 100)|round(1, 'floor') %}
+                                                           class="gradeable-custom-item-input"
+                                                           aria-label="{{gradeable["title"]}} percent"
+                                                           placeholder="{{ default_percent }}"
+                                                           value="{{ gradeable["override_percent"] ? gradeable["percent"] : default_percent }}"
+                                                           id="percents-{{ gradeable["id"] }}"
+                                                           onblur="ClampPercents(this); ClampPerGradeablePercents(this, '{{ bucket }}')">
+                                                    <span>%</span>
+                                                </div>
                                                 <div class="gradeable-li-right-of-input">
                                                     <div class="gradeable-li-info">
                                                         <i class="fas fa-exclamation-triangle fa-rb tooltip">

site/public/css/rainbow-customization.css
@@ -127,7 +127,7 @@
     flex-direction: column;
 }
 
-.gradeable-li-right-of-input i.fa-exclamation-triangle {
+i.fa-exclamation-triangle {
     margin-right: 5px;
     padding-bottom: 2px;
     display: none;
@@ -136,12 +136,12 @@
     border-bottom: 1px dotted var(--standard-vibrant-yellow);
 }
 
-[data-theme="dark"] .gradeable-li-right-of-input i.fa-exclamation-triangle {
+[data-theme="dark"] i.fa-exclamation-triangle {
     color: var(--standard-light-yellow);
     border-bottom: 1px dotted var(--standard-light-yellow);
 }
 
-.gradeable-li-right-of-input i.fa-exclamation-triangle > .tooltiptext.tooltipbottom {
+i.fa-exclamation-triangle > .tooltiptext.tooltipbottom {
     margin-left: -110px;
     margin-top: 25px;
     font-family: "Source Sans Pro", sans-serif;
@@ -150,11 +150,11 @@
     padding: 5px;
 }
 
-[data-theme="dark"] .gradeable-li-right-of-input i.fa-exclamation-triangle > .tooltiptext.tooltipbottom {
+[data-theme="dark"] i.fa-exclamation-triangle > .tooltiptext.tooltipbottom {
     color: var(--text-black);
 }
 
-.gradeable-li-right-of-input i.fa-exclamation-triangle > .tooltiptext.tooltipbottom::after {
+i.fa-exclamation-triangle > .tooltiptext.tooltipbottom::after {
     content: "";
     position: absolute;
     top: -14%;
@@ -206,7 +206,7 @@ hr {
     color: var(--standard-vibrant-orange) !important;
 }
 
-.override + div.gradeable-li-right-of-input i.fa-exclamation-triangle {
+div:has(.override) ~ div.gradeable-li-right-of-input i.fa-exclamation-triangle {
     display: inline-block;
 }
 
@@ -235,6 +235,15 @@ hr {
 
 #gradeables .gradeable-custom-item-input {
     width: 3em;
+}
+
+[id^="gradeable-pts-div-"],
+[id^="gradeable-percents-div-"] {
+    margin-right: 2em;
+}
+
+[id^="gradeable-percents-div-"] {
+    display: none;
 }
 
 #btn-build-customization {

site/public/js/rainbow-customization.js
@@ -1,4 +1,4 @@
-/* exported addToTable, deleteRow */
+/* exported addToTable, deleteRow ResetPerGradeablePercents */
 /* global buildCourseUrl csrfToken displayErrorMessage displaySuccessMessage */
 
 const benchmarks_with_input_fields = ['lowest_a-', 'lowest_b-', 'lowest_c-', 'lowest_d'];
@@ -28,7 +28,6 @@ function ClampGradeablesInBucket(el, num_gradeables) {
 }
 
 // Forces element's value to be non-negative
-// eslint-disable-next-line no-unused-vars
 function ClampPoints(el) {
     if (el.value === '') {
         el.value = el.placeholder;
@@ -37,7 +36,15 @@ function ClampPoints(el) {
     el.value = Math.max(0.0, el.value);
 }
 
-// eslint-disable-next-line no-unused-vars
+// Forces element's value to be non-negative and between 0.0 - 100.0
+// Distinct from ClampPercent(), this is for Per Gradeable Percents
+function ClampPercents(el) {
+    if (el.value === '') {
+        el.value = el.placeholder;
+    }
+    el.value = Math.min(Math.max(el.value, 0.0), 100.0);
+}
+
 function DetectMaxOverride(el) {
     if (el.value !== el.placeholder) {
         el.classList.add('override');
@@ -61,17 +68,47 @@ function ExtractBucketName(s, offset) {
 }
 
 // Forces element's value to be in range [0.0,100.0]
-// eslint-disable-next-line no-unused-vars
 function ClampPercent(el) {
     el.value = Math.min(Math.max(el.value, 0.0), 100.0);
     UpdateUsedPercentage();
     $(`#config-percent-${ExtractBucketName(el.id, 1)}`).text(`${el.value}%`);
 }
 
+// Forces sum of Per Gradeable Percents in a bucket to be below 100.0
+function ClampPerGradeablePercents(el, bucket) {
+    const percentsInputsInBucket = $(`div[id^="gradeable-percents-div-${bucket}"]`);
+    let sum = 0.0;
+
+    percentsInputsInBucket.each((index, percentInput) => {
+        const textbox = $(percentInput).children().first();
+        sum += parseFloat(textbox.val());
+    });
+
+    const warningIcon = $(`#per-gradeable-percents-warning-${bucket}`);
+    if (sum > 100.0) {
+        const excess = sum - 100.0;
+        warningIcon.show();
+        $(warningIcon.children()[0]).text(`WARNING: Per Gradeable Percents exceeds 100 by ${excess}. Do not be alarmed if this is due to Extra Credit`);
+    }
+    else {
+        warningIcon.hide();
+    }
+}
+
+// Resets Per Gradeable Percents in a given bucket to an even split
+function ResetPerGradeablePercents(bucket) {
+    const percentsInputsInBucket = $(`div[id^="gradeable-percents-div-${bucket}"]`);
+
+    percentsInputsInBucket.each((index, percentInput) => {
+        const textbox = $(percentInput).children().first();
+        textbox.val('').blur(); // If the textbox is empty, it resets to an even split onblur
+    });
+}
+
 // Updates the sum of percentage points accounted for by the buckets being used
 function UpdateUsedPercentage() {
     let val = 0.0;
-    $("input[id^='percent']").filter(function () {
+    $("input[id^='percent-']").filter(function () {
         return $(this).parent().css('display') !== 'none';
     }).each(function () {
         val += parseFloat($(this).val());
@@ -107,7 +144,6 @@ function UpdateVisibilityBuckets() {
     used_buckets.each(function () {
         // Extract the bucket name
         const bucket = ExtractBucketName($(this).attr('id'), 1);
-        console.log(`prev_bucket: ${prev_bucket} bucket: ${bucket}`);
         if (bucket !== prev_bucket) {
             $(`#config-${bucket}`).css('display', 'block');
             $(`#config-${prev_bucket}`).after($(`#config-${bucket}`));
@@ -212,9 +248,19 @@ function getGradeableBuckets() {
                 const gradeable = {};
 
                 const children = $(this).children();
+                // children[0] represents <div id="gradeable-pts-div-*">
+                // children[1] represents <div id="gradeable-percents-div-*">
+                // replace divs with inputs
+                children[0] = children[0].children[0];
+                children[1] = children[1].children[0];
 
                 // Get max points
                 gradeable.max = parseFloat(children[0].value);
+
+                // Get gradeable final grade percent, but only if Per Gradeable Percents was selected
+                if ($(children[1]).is(':visible')) {
+                    gradeable.percent = parseFloat(children[1].value) / 100.0;
+                }
 
                 // Get gradeable release date
                 gradeable.release_date = children[0].dataset.gradeReleaseDate;
@@ -951,6 +997,75 @@ $(document).ready(() => {
 
         dropLowestDivs.each((index, dropLowestDiv) => {
             $(dropLowestDiv).css('display', isChecked ? 'block' : 'none');
+        });
+    });
+
+    // Per Gradeable Percents checked on-ready if at least one Per Gradeable Percents is checked
+    const enablePerGradeablePercents = $('#enable-per-gradeable-percents');
+    const perGradeablePercentsCheckboxes = $('input[id^="per-gradeable-percents-checkbox-"]');
+    perGradeablePercentsCheckboxes.each((index, perGradeablePercentsCheckboxDOMElement) => {
+        if ($(perGradeablePercentsCheckboxDOMElement).is(':checked')) {
+            enablePerGradeablePercents.prop('checked', true);
+            return false; // Break loop
+        }
+    });
+
+    // Control visibility of per gradeable percent checkboxes
+    const perGradeablePercentsLabels = $('label[id^="per-gradeable-percents-label-"]');
+    const perGradeablePercentsReset = $('button[id^="per-gradeable-percents-reset-"]');
+    const isChecked = enablePerGradeablePercents.is(':checked');
+    perGradeablePercentsCheckboxes.each((index, checkbox) => {
+        $(checkbox).toggle(isChecked);
+    });
+    perGradeablePercentsLabels.each((index, label) => {
+        $(label).toggle(isChecked);
+    });
+    perGradeablePercentsReset.each((index, button) => {
+        if (isChecked === false) { // Only hide, otherwise element will be out of place
+            $(button).hide();
+        }
+    });
+    enablePerGradeablePercents.change(function (event) {
+        event.stopPropagation();
+        const isChecked = $(this).is(':checked');
+        perGradeablePercentsCheckboxes.each((index, checkbox) => {
+            $(checkbox).toggle(isChecked);
+        });
+        perGradeablePercentsLabels.each((index, label) => {
+            $(label).toggle(isChecked);
+        });
+        perGradeablePercentsReset.each((index, button) => {
+            if (isChecked === false) { // Only hide, otherwise element will be out of place
+                $(button).hide();
+            }
+        });
+    });
+
+    // Control visibility of per gradeable percent input boxes
+    perGradeablePercentsCheckboxes.each((index, perGradeablePercentsCheckboxDOMElement) => {
+        const perGradeablePercentsCheckbox = $(perGradeablePercentsCheckboxDOMElement);
+        const bucket = perGradeablePercentsCheckbox[0].id.match(/^per-gradeable-percents-checkbox-(.+)$/)[1];
+        const percentsInputsInBucket = $(`div[id^="gradeable-percents-div-${bucket}"]`);
+        const resetButtonInBucket = $(`button[id^="per-gradeable-percents-reset-${bucket}"]`);
+        ClampPerGradeablePercents(percentsInputsInBucket.children()[0], bucket);
+
+        const isChecked = perGradeablePercentsCheckbox.is(':checked');
+        percentsInputsInBucket.each((index, percentInput) => {
+            $(percentInput).toggle(isChecked);
+        });
+        resetButtonInBucket.each((index, resetButton) => {
+            $(resetButton).toggle(isChecked);
+        });
+
+        perGradeablePercentsCheckbox.change(function (event) {
+            event.stopPropagation();
+            const isChecked = $(this).is(':checked');
+            percentsInputsInBucket.each((index, percentInput) => {
+                $(percentInput).toggle(isChecked);
+            });
+            resetButtonInBucket.each((index, resetButton) => {
+                $(resetButton).toggle(isChecked);
+            });
         });
     });
 });

commit 7dd0f43b48fa14aa0d141d01b7f3a4310337acf8

[Bugfix:RainbowGrades] Fix error when adding gradeable (#10923)
### Please check if the PR fulfills these requirements:

* [ ] Tests for the changes have been added/updated (if possible)
* [ ] Documentation has been updated/added if relevant
* [ ] Screenshots are attached to Github PR if visual/UI changes were
made

### What is the current behavior?
<!-- List issue if it fixes/closes/implements one using the "Fixes
#<number>" or "Closes #<number>" syntax -->
After adding a gradeable to a bucket used in `customization.json`, the
following error will appear after navigating to the Grade Reports >
Web-Based GUI Customization.
![error after adding
gradeable](https://github.com/user-attachments/assets/743721c6-6db3-47c4-adcd-a406d587b814)

Steps to Reproduce

1. Login to Instructor, then navigate to Grade Reports > Web-Based
Rainbow Grades Customization.
2. Drag any item from **Available Buckets** to **Assigned Buckets**. 
3. Navigate to New Gradeable, and create a gradeable in the same bucket
as used in the previous step.
4. Return to Web-Based Rainbow Grades Customization, and view error.

### What is the new behavior?
The new gradeable will no longer crash the customization page. If Per
Gradeable Percents is enabled, the Per Gradeable Percent of the new
gradeable will be the inverse of the number of gradeables in the bucket.

### Other information?
<!-- Is this a breaking change? -->
<!-- How did you test -->
This error was introduced in PR #10763 (Add Gradeable Percents to GUI)

site/app/models/RainbowCustomization.php
@@ -156,7 +156,7 @@ class RainbowCustomization extends AbstractModel {
                         $c_gradeable['override'] = true;
                         $c_gradeable['override_max'] = $json_bucket->ids[$j_index]->max;
                     }
-                    if (property_exists($json_bucket->ids[$j_index], 'percent')) {
+                    if (isset($json_bucket->ids[$j_index]) && property_exists($json_bucket->ids[$j_index], 'percent')) {
                         $c_gradeable['override_percent'] = true;
                         $c_gradeable['percent'] = ($json_bucket->ids[$j_index]->percent) * 100;
                     }